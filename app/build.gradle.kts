54bb3ab853efb639c1c3415c14f6de102c8cc214
Charly <root@charly-tricks.dev>
2026-08-02 18:44:33 +0200

    subir versionCode a 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.retroquiz"
         minSdk = 21
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 
     signingConfigs {
